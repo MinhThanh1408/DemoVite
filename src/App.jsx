51944460db745eff054b8662f50d10285fa9571b
MinhThanh_MSI <minhthanh14032003@gmail.com>
2023-04-21 14:51:53 +0700

    add css avatar

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
       <img
         src='https://scontent.fsgn15-1.fna.fbcdn.net/v/t39.30808-1/337701901_3412911172299573_6337026887903548466_n.jpg?stp=dst-jpg_s200x200&_nc_cat=108&ccb=1-7&_nc_sid=7206a8&_nc_ohc=VUGamRN-zkYAX94_0sV&_nc_ht=scontent.fsgn15-1.fna&oh=00_AfB8NRLYKImodgJ5vvLTU-mt3bAxFoEh6JH13eIiY09_4g&oe=6447FCCD'
         alt=''
+        className='avatar-vy'
       />
       <h1>Avatar của con Vy</h1>
     </>
